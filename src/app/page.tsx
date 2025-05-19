f534e07a304244ae128a12d8ab3cf2359af212c4
mjkohan <mjk1551381@gmail.com>
2025-05-19 11:21:46 +0330

    rendering qs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
+'use client'
+import {useQuestionnaireStore} from "@/store/questionnaire";
+import {Box} from "@mui/system";
+import {Paper, Typography} from "@mui/material";
+import {conditions, questions} from "@/constants/questions";
+import {shouldDisplay} from "@/utils/conditionEvaluator";
 
 export default function Home() {
-  return (
+    const { answers } = useQuestionnaireStore()
+
+    return (
     <div >
       <main >
+          <Box maxWidth="600px" mx="auto" mt={4}>
+              <Typography variant="h5" gutterBottom>
+                  پرسشنامه مرحله‌ای
+              </Typography>
 
+              {questions.map((question) =>
+                  shouldDisplay(question.id, conditions, answers) ? (
+                      <Paper key={question.id} sx={{ p: 2, mb: 3 }}>
+                          {question.title}
+                      </Paper>
+                  ) : null
+              )}
+          </Box>
       </main>
 
     </div>
